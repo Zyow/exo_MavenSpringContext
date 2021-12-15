7aa30d0d6c0d92bcaa3c50d82b5d88410863de3e
Zyow <Szczypamaxime@gmail.com>
2021-12-15 11:40:59 +0100

    firt commit

---
+++ src/main/java/fr/mxsz/exo_MavenSpringContext/ExoMavenSpringContextApplication.java
@@ -0,0 +1,13 @@
+package fr.mxsz.exo_MavenSpringContext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoMavenSpringContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExoMavenSpringContextApplication.class, args);
+	}
+
+}
